2590a0c15049890a81829553d7129c1f6410738a
Tristram Gräbener <tristramg@gmail.com>
2023-06-18 15:54:19 +0200

    generate global outlines

--- src/app/map.tsx
+++ src/app/map.tsx
@@ -4,8 +4,9 @@ import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import styles from './page.module.css'
 import troncons from '../../data/vif.json'
-import {featureCollection, lineString, multiLineString} from '@turf/helpers'
+import { featureCollection, lineString, multiLineString } from '@turf/helpers'
 import _ from 'lodash'
+import outlines from '../utils/outlines'
 
 export default function Map() {
     const mapContainer = useRef<null | HTMLElement>(null);
@@ -17,69 +18,63 @@ export default function Map() {
     useEffect(() => {
         if (map.current) return;
 
+        const outlineGeojson = featureCollection(outlines())
 
-        // This is a failed attempt to see if creating a single multiLineString per line
-        // we could avoid having line-caps between successive tronçons of the same line
-        const outlineGeojson = featureCollection(_(troncons.features)
-            .reject(['properties.NIVEAU_VALID_SUPPORT_VIAIRE', 'Variante'])
-            .groupBy('properties.NUM_LIGNE')
-            .values()
-            .map(e => multiLineString(_.flatMap(e, 'geometry.coordinates')))
-            .value())
-
-      const newMap = new maplibregl.Map({
-          container: mapContainer.current || '',
-          style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`,
-          center: [lng, lat],
-          zoom: zoom
+        const newMap = new maplibregl.Map({
+            container: mapContainer.current || '',
+            style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`,
+            center: [lng, lat],
+            zoom: zoom
         }).on('load', () => {
-          newMap.addSource('vif', {type: 'geojson', data: troncons})
-                .addSource('outline', {type: 'geojson', data: outlineGeojson})
-          .addLayer({
-            id: 'base',
-            source: 'vif',
-            type: 'line',
-            paint: {
-                'line-width': ["interpolate", ["linear"], ["zoom"], 10, 1, 15, 3],
-                'line-gap-width': ["interpolate", ["linear"], ["zoom"], 10, 3, 15, 10],
-                'line-opacity': 0.5,
-            },
-            layout: {
-                'line-join': 'round',
-                'line-cap': 'round',
-            },
-            filter: ['!=', ['get', 'NIVEAU_VALID_SUPPORT_VIAIRE'], 'Variante']
+            newMap.addSource('vif', { type: 'geojson', data: troncons })
+                .addSource('outline', { type: 'geojson', data: outlineGeojson })
+                .addLayer({
+                    id: 'base',
+                    source: 'outline',
+                    type: 'line',
+                    paint: {
+                        'line-width': ["interpolate", ["linear"], ["zoom"], 10, 1, 15, 3],
+                        'line-gap-width': ["interpolate", ["linear"], ["zoom"], 10, 3, 15, 10],
+                        'line-opacity': 1,
+                        'line-color': '#555'
+                    },
+                    layout: {
+                        'line-join': 'round',
+                        'line-cap': 'round',
+                    },
+                    filter: ['!=', ['get', 'NIVEAU_VALID_SUPPORT_VIAIRE'], 'Variante']
+                })
+                .addLayer({
+                    id: 'couleur',
+                    source: 'vif',
+                    type: 'line',
+                    paint: {
+                        'line-width': ["interpolate", ["linear"], ["zoom"], 10, 2, 15, 6],
+                        'line-color': ['match', ['get', 'NIVEAU_VALID_AMENAG'],
+                            'Mis en service', '#3A3',
+                            'En travaux', '#33A',
+                            '#A33'
+                        ]
+                    },
+                    layout: {
+                        'line-cap': 'round',
+                        'line-join': 'round',
+                    },
+                    filter: ['!=', ['get', 'NIVEAU_VALID_SUPPORT_VIAIRE'], 'Variante']
+                })
+                .addLayer({
+                    id: 'variantes',
+                    source: 'vif',
+                    type: 'line',
+                    paint: {
+                        'line-width': ["interpolate", ["linear"], ["zoom"], 10, 1, 15, 3],
+                        'line-dasharray': [2, 1],
+                    },
+                    filter: ['==', ['get', 'NIVEAU_VALID_SUPPORT_VIAIRE'], 'Variante'],
+                });
         })
-        .addLayer({
-            id: 'couleur',
-            source: 'vif',
-            type: 'line',
-            paint: {
-                'line-width': ["interpolate", ["linear"], ["zoom"], 10, 2, 15, 6],
-                'line-color': ['match', ['get', 'NIVEAU_VALID_AMENAG'],
-                    'Mis en service', '#3A3',
-                    'En travaux', '#33A',
-                    '#A33'
-                ]
-            },
-            layout: {
-                'line-cap': 'round',
-            },
-            filter: ['!=', ['get', 'NIVEAU_VALID_SUPPORT_VIAIRE'], 'Variante']
-        })
-        .addLayer({
-            id: 'variantes',
-            source: 'vif',
-            type: 'line',
-            paint: {
-                'line-width': ["interpolate", ["linear"], ["zoom"], 10, 1, 15, 3],
-                'line-dasharray': [2, 1],
-            },
-            filter: ['==', ['get', 'NIVEAU_VALID_SUPPORT_VIAIRE'], 'Variante'],
-        });
-      })
 
-      map.current = newMap;
+        map.current = newMap;
     });
 
     return (
