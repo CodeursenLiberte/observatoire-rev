02b28a411938bbf5b7ba9c88ac2fac157a41b9f6
Tristram Gräbener <tristramg@gmail.com>
2023-06-23 15:39:26 +0200

    style a bit the department’s list

--- src/app/component/departement_stats.tsx
+++ src/app/component/departement_stats.tsx
@@ -1,19 +1,15 @@
 export default function DepartementStats({name, code, progress}: {name: string, code: string, progress: number}) {
     return(
-    <nav className="level">
-      <div className="level-left">
-        <div className="level-item has-text-centered">
+    <div className="columns is-mobile is-vcentered">
+      <div className="column is-2 is-size-3 pr-0">
           <a className="link is-info">{code}</a>
-        </div>
       </div>
-      <div className="level-right">
-        <div className="level-item">
-          <p className="heading">{name}</p>
-          <p>
-            <progress className="progress" value="15" max="100">{progress}%</progress>
-          </p>
-        </div>
+      <div className="column has-text-left pl-0">
+        <p className="heading is-size-6 mb-0">{name}</p>
+        <p>
+          <progress className="progress" value="15" max="100">{progress}%</progress>
+        </p>
       </div>
-    </nav>
+    </div>
     )
 }
