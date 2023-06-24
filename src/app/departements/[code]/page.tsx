dc8c5aba5665246afadb10a4c043fda4e090325d
Tristram Gräbener <tristramg@gmail.com>
2023-06-24 23:22:14 +0200

    Récupération de données sur le département

--- src/app/departements/[code]/page.tsx
+++ src/app/departements/[code]/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import departements from "@/utils/prepared_departements"
+import { GetStaticProps } from "next";
+import { Departement } from "@/app/types";
 
 export async function generateStaticParams() {
   return departements().map((departement) => ({
@@ -7,10 +9,12 @@ export async function generateStaticParams() {
   }))
 }
 
-export default function Departement({ params }: { params: { code: string } }) {
+export default function DepartementDetail({ params }: { params: { code: string } }) {
+  const departement = departements().find(dep => dep.code === params.code)!;
   return <section className="section has-text-centered">
     <div className="content">
-      <h2 className="title is-3">Département du {params.code}</h2>
+      <h2 className="title is-3">Département {departement.name} ({params.code})</h2>
+      <p> {Math.round(departement.stats.built/1000)} km de voies construites sur {Math.round(departement.stats.total/1000)}</p>
       <Link href="/">retour</Link>
     </div>
   </section>
