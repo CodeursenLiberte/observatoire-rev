20c681326f7d8bf9d45a5a8a1e303e18ab2d0e7c
Tristram Gräbener <tristramg@gmail.com>
2023-07-09 22:25:05 +0200

    style the route buttons

--- src/app/component/routes_list.tsx
+++ src/app/component/routes_list.tsx
@@ -5,11 +5,13 @@ export default function RouteList({routes}: {routes: RoutesMap}) {
   return <section className="section has-text-centered route-list">
     <div className="container">
       <h2 className="title is-3">Informations par&nbsp;ligne</h2>
+      <div className="route-list-icons">
         {Object.keys(routes).map(route =>
             <Link key={route} href={`?level=route&id=${route}`}>
-              <button className="button">{route}</button>
+              <button className="button route-button">{route}</button>
             </Link>
         )}
+      </div>
     </div>
   </section>
 }
