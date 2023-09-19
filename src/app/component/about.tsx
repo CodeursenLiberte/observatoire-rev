e27b003a184576b706e7a44a527db16c6123431f
Nicolas Bouilleaud <nico@bou.io>
2023-09-19 16:24:03 +0200

    Tweak About text and put logo above the map

--- src/app/component/about.tsx
+++ src/app/component/about.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <div className="vif-container vif-container--about">
         <h2 className="title is-size-4 has-text-centered">À propos</h2>
         <p className="block has-text-centered">
-          Cet observatoire est proposé et maintenu par le
+          Cet observatoire est proposé et maintenu par le{" "}
           <Link href="https://velo-iledefrance.fr/">
             Collectif Vélo Île-de-France
           </Link>
