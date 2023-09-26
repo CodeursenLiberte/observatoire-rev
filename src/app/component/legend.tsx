af0edafb402931db1360c38c23346c95be073720
Nicolas Bouilleaud <nico@bou.io>
2023-09-26 17:20:45 +0200

    Est-il possible d’afficher les pourcentages d’avancement à côté de chaque indication de légende ?

--- src/app/component/legend.tsx
+++ src/app/component/legend.tsx
@@ -1,7 +1,15 @@
 import { TronçonStatus, LengthStats } from "../types";
 import { statusColor, statusLabel } from "@/utils/constants";
 
-function LegendItem({ stats, total, status }: { stats: LengthStats, total: number, status: TronçonStatus }) {
+function LegendItem({
+  stats,
+  total,
+  status,
+}: {
+  stats: LengthStats;
+  total: number;
+  status: TronçonStatus;
+}) {
   const style = {
     background: statusColor[status],
     border: "none",
@@ -11,22 +19,38 @@ function LegendItem({ stats, total, status }: { stats: LengthStats, total: numbe
   }
   return (
     <div>
-      <span className="legend-value">{Math.round((100 * stats[status]) / total)}%</span>
+      <span className="legend-value">
+        {Math.round((100 * stats[status]) / total)}%
+      </span>
       <span style={style} className="legend-color" />
       <span>{statusLabel[status]}</span>
     </div>
   );
 }
 
-export default function Legend({stats, total}: { stats: LengthStats, total: number}) {
+export default function Legend({
+  stats,
+  total,
+}: {
+  stats: LengthStats;
+  total: number;
+}) {
   return (
     <>
-      <LegendItem stats={stats} total={total} status={TronçonStatus.PreExisting} />
+      <LegendItem
+        stats={stats}
+        total={total}
+        status={TronçonStatus.PreExisting}
+      />
       <LegendItem stats={stats} total={total} status={TronçonStatus.Built} />
       <LegendItem stats={stats} total={total} status={TronçonStatus.Building} />
       <LegendItem stats={stats} total={total} status={TronçonStatus.Planned} />
       <LegendItem stats={stats} total={total} status={TronçonStatus.Blocked} />
-      <LegendItem stats={stats} total={total} status={TronçonStatus.SecondPhase} />
+      <LegendItem
+        stats={stats}
+        total={total}
+        status={TronçonStatus.SecondPhase}
+      />
       <LegendItem stats={stats} total={total} status={TronçonStatus.Unknown} />
     </>
   );
